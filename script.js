aa3bec14731a83cd2e581cf34ae05531cebd47e6
Tony Km <kimaxhanthony9@gmail.com>
2023-04-03 23:18:35 +0300

    Quiz App

---
+++ script.js
@@ -0,0 +1,100 @@
+const quizAndAnswers = [
+    {
+        question: "How many countries are there in Africa",
+        A: "57",
+        B: "53",
+        C: "63",
+        D: "47",
+        correctAns: "B"
+    },
+    {
+        question: "Which one of these is wrong",
+        A: "Python is used in data analysis",
+        B: "Javascript is for web development",
+        C: "Java is a styling language",
+        D: "Python is used for web development",
+        correctAns: "C"
+    },
+    {
+        question: "What is the most popular programming problem?",
+        A: "Missing a comma",
+        B: "Missing a Semicolon",
+        C: "Missing a colon",
+        D: "Missing a fullstop",
+        correctAns: "B"
+    },
+    {
+        question: "What two words every programmer learned to code first?",
+        A: "If else",
+        B: "Print line",
+        C: "Hello world",
+        D: "Hello",
+        correctAns: "D"
+    },
+    {
+        question: "Which one is the scripting language for web development",
+        A: "HTML",
+        B: "Javascript",
+        C: "CSS",
+        D: "Python",
+        correctAns: "C"
+    }
+];
+
+const container = document.querySelector(".main-container")
+const question = document.getElementById("quizes");
+const ansA = document.getElementById("a");
+const ansB = document.getElementById("b");
+const ansC = document.getElementById("c");
+const ansD = document.getElementById("d");
+const submitButton = document.getElementById("btn");
+const answers = document.querySelectorAll(".answers");
+
+let currentQuestion = 0;
+let results = 0;
+
+loadQuizes()
+function loadQuizes(){
+    deselectPreviousAns();
+    const currQuiz = quizAndAnswers[currentQuestion];
+    question.innerText = currQuiz.question;
+
+    ansA.innerText = currQuiz.A;
+    ansB.innerText = currQuiz.B;
+    ansC.innerText = currQuiz.C;
+    ansD.innerText = currQuiz.D;
+}
+function selectedOne(){
+    let ans;
+    answers.forEach(answer => {     
+        if(answer.checked){
+            ans = answer.id;
+        }
+    });
+    return ans;
+}
+
+function deselectPreviousAns(){
+    answers.forEach(answer => {
+        answer.checked = false
+    });
+}
+
+submitButton.addEventListener("click", ()=>{
+    const ans = selectedOne();
+
+    if(ans){
+        if(ans === quizAndAnswers[currentQuestion].correctAns){          
+            // results++;
+            console.log(results++)
+        };
+        currentQuestion++;
+        if(currentQuestion < quizAndAnswers.length){
+            loadQuizes();
+        }else{
+            container.innerHTML = `<h2>You scored ${results} out of 5<h2/>`
+        } 
+    }
+    
+});
+
